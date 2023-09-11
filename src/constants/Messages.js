6b7f1ed9a08c1f492562752cb0b6b4a13146adc4
yui880 <kimyuna521@gmail.com>
2023-09-11 18:37:28 +0900

    feat : 게임에서 사용자에게 출력될 메세지 상수 작성

---
+++ src/constants/Messages.js
@@ -0,0 +1,11 @@
+const Messages = {
+  GAME_START: '숫자 야구 게임을 시작합니다',
+  ENTER_NUMBER_INPUT: '숫자를 입력해주세요 : ',
+  BALL: '볼',
+  STRIKE: '스트라이크',
+  NOTHING: '낫싱',
+  ANSWER_CORRECTLY: '3개의 숫자를 모두 맞히셨습니다! 게임 종료',
+  ASK_TO_RESTART: '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.',
+};
+
+module.exports = Messages;
