25afd839d1d8d05bae5e492b1a2ced569585b227
yui880 <kimyuna521@gmail.com>
2023-09-12 16:16:54 +0900

    feat : 입력된 수가 숫자인지 확인하는 함수 구현

--- src/Exception.js
+++ src/Exception.js
@@ -2,6 +2,10 @@ class Exception {
   static checkIsThreeDigitNumber(number) {
     return number.length === 3;
   }
+
+  static checkIsNumber(number) {
+    return !Number.isNaN(number);
+  }
 }
 
 module.exports = Exception;
