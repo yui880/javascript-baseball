d73d4841640a9cd699d38746ae318849a0ea1fa1
yui880 <kimyuna521@gmail.com>
2023-10-15 18:25:28 +0900

    feat : 에러가 발생하는 예외 입력값에 대한 테스트 작성

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -1,5 +1,6 @@
-const App = require("../src/App");
-const MissionUtils = require("@woowacourse/mission-utils");
+const MissionUtils = require('@woowacourse/mission-utils');
+const App = require('../src/App');
+const { ERROR } = require('../src/constants/Constant');
 
 const mockQuestions = (answers) => {
   MissionUtils.Console.readLine = jest.fn();
@@ -18,23 +19,17 @@ const mockRandoms = (numbers) => {
 };
 
 const getLogSpy = () => {
-  const logSpy = jest.spyOn(MissionUtils.Console, "print");
+  const logSpy = jest.spyOn(MissionUtils.Console, 'print');
   logSpy.mockClear();
   return logSpy;
 };
 
-describe("숫자 야구 게임", () => {
-  test("게임 종료 후 재시작", () => {
+describe('숫자 야구 게임', () => {
+  test('게임 종료 후 재시작', () => {
     const randoms = [1, 3, 5, 5, 8, 9];
-    const answers = ["246", "135", "1", "597", "589", "2"];
+    const answers = ['246', '135', '1', '597', '589', '2'];
     const logSpy = getLogSpy();
-    const messages = [
-      "낫싱",
-      "3스트라이크",
-      "1볼 1스트라이크",
-      "3스트라이크",
-      "게임 종료",
-    ];
+    const messages = ['낫싱', '3스트라이크', '1볼 1스트라이크', '3스트라이크', '게임 종료'];
 
     mockRandoms(randoms);
     mockQuestions(answers);
@@ -47,9 +42,74 @@ describe("숫자 야구 게임", () => {
     });
   });
 
-  test("예외 테스트", () => {
+  test('입력값이 숫자가 아닐 때', () => {
+    const randoms = [2, 3, 4];
+    const answers = ['12j'];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    expect(() => {
+      const app = new App();
+      app.play();
+    }).toThrow(new Error(ERROR.NOT_A_NUMBER));
+  });
+
+  test('입력값이 세자리가 아닐 때', () => {
+    const randoms = [2, 3, 4];
+    const answers = ['1278'];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    expect(() => {
+      const app = new App();
+      app.play();
+    }).toThrow(new Error(ERROR.NOT_A_THREE_DIGIT));
+  });
+
+  test('입력값이 0을 포함할 때', () => {
+    const randoms = [2, 3, 4];
+    const answers = ['102'];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    expect(() => {
+      const app = new App();
+      app.play();
+    }).toThrow(new Error(ERROR.HAVE_ZERO));
+  });
+
+  test('입력값에 중복된 숫자가 있을 때', () => {
+    const randoms = [1, 2, 3];
+    const answers = ['122'];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    expect(() => {
+      const app = new App();
+      app.play();
+    }).toThrow(new Error(ERROR.HAVE_SAME_NUMBER));
+  });
+
+  test('게임종료시 1이나 2가 아닌 입력이 들어왔을 때', () => {
+    const randoms = [1, 2, 3, 4, 5, 6];
+    const answers = ['123', '1', '456', '3'];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    expect(() => {
+      const app = new App();
+      app.play();
+    }).toThrow(new Error(ERROR.IS_NOT_ONE_OR_TWO));
+  });
+
+  test('예외 테스트', () => {
     const randoms = [1, 3, 5];
-    const answers = ["1234"];
+    const answers = ['1234'];
 
     mockRandoms(randoms);
     mockQuestions(answers);
